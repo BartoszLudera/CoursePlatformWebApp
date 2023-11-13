4b3bdab195450f49e99e9714349b81513913b153
BLudera <bartek.ludera@gmail.com>
2023-11-14 00:14:13 +0100

    adding code API

---
+++ course-platform-web-app/src/components/Command.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Command = () => {
+    return (
+        <>
+            <h1 className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
+                Command with your exercise
+            </h1>
+            <div className="w-full h-auto bg-whtie rounded-md text-[#1e293b] font-normal text-sm overflow-y-auto mb-6">
+                Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+            </div>
+        </>
+    );
+};
+
+export default Command;
---
+++ course-platform-web-app/src/components/CustomInput.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { classnames } from "../utils/general";
+
+const CustomInput = ({ customInput, setCustomInput }) => {
+  return (
+    <>
+      {" "}
+      <textarea
+        rows="5"
+        value={customInput}
+        onChange={(e) => setCustomInput(e.target.value)}
+        placeholder={`Custom input`}
+        className={classnames(
+          "focus:outline-none w-full border-2 border-black z-10 rounded-md hover:bg-black hover:text-white mt-2"
+        )}
+      ></textarea>
+    </>
+  );
+};
+
+export default CustomInput;
